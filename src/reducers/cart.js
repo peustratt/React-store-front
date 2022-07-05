52d897e19731e05701eb533f7e380655eb97c50e
Peu <peustratt@gmail.com>
2022-07-05 00:22:46 -0300

    added comments

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -49,10 +49,12 @@ const cartReducer = (state = { products: [], total: 0, currentCurrency: {} }, ac
       let newSelected = oldProduct.selectedAttributes.map(attribute => {
         return attribute.attributeId === action.payload.attributeId ? { ...attribute, itemId: action.payload.newItemId } : attribute
       })
-      console.log('eai é ou não é', JSON.stringify(action.payload.selectedAttributes) !== JSON.stringify(newSelected))
+
+      // check if selectedAttributes have changed
       if (JSON.stringify(action.payload.selectedAttributes) !== JSON.stringify(newSelected)) {
         duplicated = state.products.find(product => product.id === action.payload.productId && JSON.stringify(product.selectedAttributes) === JSON.stringify(newSelected))
-        console.log('duplicate', duplicated)
+
+        // if is duplicated product.quantity++ and remove old product
         if (duplicated) {
           const newState = [...state.products];
           let duplicatedId = newState.indexOf(duplicated);
@@ -72,11 +74,14 @@ const cartReducer = (state = { products: [], total: 0, currentCurrency: {} }, ac
       let selectedProduct = state.products.find((product) => product.id === action.payload.product.id && JSON.stringify(product.selectedAttributes) === JSON.stringify(action.payload.product.selectedAttributes))
       price = selectedProduct.prices.find(price => price.currency.label === state.currentCurrency.label)
 
+      // Remove product from cart
       if (operator < 0 && selectedProduct?.quantity === 1) {
         const newState = [...state.products];
         const productIndex = newState.indexOf(selectedProduct);
         newState.splice(productIndex, 1);
         return { ...state, products: newState, total: getCartTotal(state.currentCurrency.label, newState) };
+
+        // change product quantity
       } else {
         const newState = state.products.map(product => product === selectedProduct ? { ...product, quantity: product.quantity + operator } : product)
         return { ...state, total: getCartTotal(state.currentCurrency.label, newState), products: newState }
