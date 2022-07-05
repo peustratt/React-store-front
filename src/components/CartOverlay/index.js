f3df0a70df71d87944f605feb83e026cd70586e2
Peu <peustratt@gmail.com>
2022-07-05 01:12:48 -0300

    round CartOverlay total

--- src/components/CartOverlay/index.js
+++ src/components/CartOverlay/index.js
@@ -14,7 +14,7 @@ class CartOverlay extends Component {
         return (
             <OverlayContainer>
                 {productsEL}
-                <div className="cart-total"><span>Total</span><span className="price">{this.props.currentCurrency.symbol}{this.props.cart.total}</span></div>
+                <div className="cart-total"><span>Total</span><span className="price">{this.props.currentCurrency.symbol}{Math.round(this.props.cart.total * 100) / 100}</span></div>
                 <div className="overlay-btns">
                     <Link to="/cart" onClick={() => this.props.handleOverlay('close')} className="view-bag-btn">View bag</Link>
                     <Link to="/cart" onClick={() => this.props.handleOverlay('close')} className="checkout-btn">Checkout</Link>
