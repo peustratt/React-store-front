8d153d2459b01d9c75f61f156bf07328deaa0ff5
Peu <peustratt@gmail.com>
2022-07-04 21:39:18 -0300

    created CartPage components

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import {connect } from 'react-redux';
+
+import CartPage from "./style";
+
+class Cart extends Component {
+  render() {
+    return <CartPage>Hellor products</CartPage>
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    cart: state.cart
+  }
+}
+
+export default connect(mapStateToProps, null)(Cart)
---
+++ src/components/Cart/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const CartPage = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export default CartPage;
